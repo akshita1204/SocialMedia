54ea6b47d96e71ad92a4e8a542d6908291148c9a
akshita1204 <akshitatyagi04@gmail.com>
2024-12-05 10:44:51 +0530

    basic UI

--- SocialMediaApp/src/App.jsx
+++ SocialMediaApp/src/App.jsx
@@ -2,9 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
 
 function App() {
-  return <div>Social Media</div>
+  return (
+    <div className="appcontainer">
+      <Sidebar></Sidebar>
+      <div className='content'>
+      <Header></Header>
+      <CreatePost></CreatePost>
+      <PostList></PostList>
+      <Footer></Footer>
+      </div>
+    
+    </div>
+  ); 
 }
 
 export default App
